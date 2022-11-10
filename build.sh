2fe1787c8751e27a935e4da85f1cfdd41df327ce
feknall <h.fazli.k@gmail.com>
2022-11-10 12:48:02 -0500

    Modify running scripts, increase inbound message size of gRPC

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+./gradlew build
 docker build -t dist-fed-gateway . -f Dockerfile
 docker tag dist-fed-gateway hmaid/hyperledger:dist-fed-gateway
 docker push hmaid/hyperledger:dist-fed-gateway
--- src/main/java/com/example/fljavagateway/common/Settings.java
+++ src/main/java/com/example/fljavagateway/common/Settings.java
@@ -93,7 +93,7 @@ public abstract class Settings {
         return NettyChannelBuilder.forTarget(getPeerEndpoint())
                 .sslContext(GrpcSslContexts.forClient().trustManager(tlsCert).build())
                 .overrideAuthority(overrideAuth)
-                .maxInboundMessageSize(4 * 4194304)
+                .maxInboundMessageSize(20 * 4194304)
                 .build();
     }
 
